8f2f21ff554ac9f33786ca75bf85d9191325f282
KiRiLL625 <70346582+KiRiLL625@users.noreply.github.com>
2023-06-10 02:55:45 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(1, 5.0);
+        bst.insert(2, 6.0);
+        bst.insert(3, 7.0);
+        bst.insert(4, 4.0);
+        bst.insert(5, 3.0);
+        bst.insert(6, 3.5);
+        bst.insert(7, 2.0);
+        bst.print();
+    }
+}
